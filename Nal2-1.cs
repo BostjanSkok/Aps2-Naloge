26125f71c2b1eb94dc73d424e29a882bae2bbd23
BostjanSkok <BostjanSkok2@gmail.com>
2015-04-11 14:30:48 +0200

    Added generic support as long as T equals is implemented

--- Nal2-1.cs
+++ Nal2-1.cs
@@ -6,42 +6,40 @@ void Main()
 
 	var inOrder = new int[]{2,4,5,6,7,8,15,23,45,47,50,71};
 	var preOrder = new int[]{15,6,4,2,5,7,8,23,71,50,45,47};
-	//Draw naloga c
 	var inOrderC = new int[]{22, 16, 14, 10, 2, 8, 25, 7, 1, 6, 42, 5, 19, 40, 3, 33, 15, 39};
 	var preOrderC= new int[]{7, 16, 22, 8, 14, 2, 10, 25, 1, 19, 42, 6, 5, 33, 40, 3, 39, 15};
-	var root = BuildTree(preOrderC,inOrderC);
-//	var root = BuildTree(preOrder,inOrder);
+	var root = BuildTree<int>(preOrderC,inOrderC);
 	Draw<int>(root,@"C:\Users\BostjanSkok\Documents\New folder (4)\BinaryTreeNode.dgml");
 }
-private BinaryTreeNode<int> BuildTree(int[] preOrd, int[] inOrd)
+private BinaryTreeNode<T> BuildTree<T>(T[] preOrd, T[] inOrd)
 {
   return BuildTreeR(inOrd, 0, inOrd.Length - 1, preOrd, 0);
 }
 
-private BinaryTreeNode<int> BuildTreeR(int[] inOrd, int inStart, int inEnd, int[] preOrd, int preStart)
+private BinaryTreeNode<T> BuildTreeR<T>(T[] inOrd, int inStart, int inEnd, T[] preOrd, int preStart)
 {
-   //found leaf return node without children
-    if (inStart == inEnd) return new BinaryTreeNode<int>(inOrd[inStart]);
+    //found leaf return node without children
+    if (inStart == inEnd) return new BinaryTreeNode<T>(inOrd[inStart]);
     // handled all nodes in subtree
     if (inStart > inEnd) return null;
 
     int indexOfRoot;
-    int root = preOrd[preStart];
+    T root = preOrd[preStart];
     //search for root index in inOrder table
     for (indexOfRoot = inStart; indexOfRoot < inEnd; indexOfRoot++) 
-        if (inOrd[indexOfRoot] == root) break;
+        if (root.Equals(inOrd[indexOfRoot])) break;
 
-    return new BinaryTreeNode<int>
+    return new BinaryTreeNode<T>
     {
         Value = root,
         //Build left subtree 
         //inOrder elements to handle form 0 up to index of found root
         // preOrder move start of virtual table up by 1 so preOrd[preStart] will be the next root
-        Left = BuildTreeR(inOrd, inStart, indexOfRoot - 1, preOrd, preStart + 1),
+        Left = BuildTreeR<T>(inOrd, inStart, indexOfRoot - 1, preOrd, preStart + 1),
         //Build Right subtree
         //inOrder elements to handle run from found root index +1 to end of table
         //preOrder move start up by the number of elements in left subTree using index of root
-        Right = BuildTreeR(inOrd, indexOfRoot + 1, inEnd, preOrd,
+        Right = BuildTreeR<T>(inOrd, indexOfRoot + 1, inEnd, preOrd,
         preStart + indexOfRoot - inStart + 1),
     };
 }
